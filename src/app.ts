f2cb9267b58ab37c0c40f0df7ddf3be6dc4d93bf
Utkarsh Dixit <utkarshdix02@gmail.com>
2024-07-26 23:11:41 +0530

    chore: add .env

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 
 import dotenv from "dotenv";
-dotenv.config({ path: "../.env" });
+dotenv.config();
 
 import { fromGithub, getBranchNameFromIssue } from './utils';
 import { initSWEAgent } from './agents/swe';
